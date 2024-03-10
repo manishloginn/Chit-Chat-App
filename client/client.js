b3cd23cfa6578d380015857797ec1aa6d6c07fec
Manish Verma <manishv7714@gmail.com>
2024-03-10 19:16:56 +0530

    final

--- client/client.js
+++ client/client.js
@@ -10,6 +10,10 @@ let username = prompt('name');
 
 sendButton.addEventListener('click', (e)=> {
     e.preventDefault();
+    if (!msgInput.value) {
+       return msgInput.focus()
+    }
+    
     let data = {
         id: socket.id,
         username : username,
@@ -21,30 +25,61 @@ sendButton.addEventListener('click', (e)=> {
 })
 
 
-function removeElement() {
-    const myMsg = document.querySelector(".sent");
-    myMsg.addEventListener('dblclick', (e) => {
-         e.target.remove()
-    });
-}
+
 
 
 function rendermsg(data, typeOfMsg) {
     const msgdiv = document.createElement("div");
-    const span = document.createElement("span");
-    const paragraph = document.createElement("p");
-    paragraph.innerText = `${data.username} : ${data.message} `
-    span.innerText = data.time;
+    const paragraph = document.createElement("span");
+    const time = document.createElement("span");
+    time.innerText = `${data.time} `;
+    const iconElement = document.createElement("span");
+    iconElement.setAttribute ("class", "material-symbols-outlined");
+    iconElement.setAttribute("id", "icon")
+    iconElement.textContent = 'expand_more';
+    const buttondiv = document.createElement("div");
+    const divbutton = document.createElement("button");
+    divbutton.innerText = "Delete"
+    divbutton.setAttribute("class", "deletebutton");
+
+
     if (typeOfMsg == 'SENT') {
         msgdiv.setAttribute('class', 'message sent') 
+        buttondiv.setAttribute('class', 'none sent')
+        paragraph.innerText = `${data.message} `;
+        paragraph.setAttribute("class", "textmessage")
     } else {
-        msgdiv.setAttribute('class', 'message reciev') 
+        msgdiv.setAttribute('class', 'message reciev');
+        buttondiv.setAttribute('class', 'none reciev');
+        paragraph.setAttribute("class", "textmessage");
+        paragraph.innerText = ` ${data.username} : ${data.message} `
     }
+    buttondiv.appendChild(divbutton);
+    msgdiv.appendChild(buttondiv);
     msgdiv.appendChild(paragraph);
-    msgdiv.appendChild(span);
+    paragraph.appendChild(time);
+    paragraph.appendChild(iconElement);
     messagecontainerr.appendChild(msgdiv);
     msgInput.value = "";
-    removeElement()
+    
+    const icon = iconElement;
+    icon.addEventListener('click', ()=> {
+        buttondiv.classList.toggle('none');
+        buttondiv.classList.toggle('show');
+    })
+
+    const deletebuttons = document.querySelectorAll(".deletebutton"); 
+     
+    deletebuttons.forEach(deletebutton => {
+        deletebutton.addEventListener('click', (e)=> {
+            const messageElement = e.target.parentNode.nextElementSibling; 
+            messageElement.setAttribute("class", "messageElement")
+            messageElement.innerText = "Message Deleted...!"
+            buttondiv.classList.remove('show');
+            buttondiv.classList.add('none');
+        })
+    })
+
 }
 
 socket.on('spreadmsg', data => {
