9b3b62512a5ee6822bbcc88ebe057213944c896c
Manish Verma <manishv7714@gmail.com>
2024-03-10 15:31:57 +0530

    final

--- client/client.js
+++ client/client.js
@@ -14,22 +14,37 @@ sendButton.addEventListener('click', (e)=> {
         id: socket.id,
         username : username,
         message : msgInput.value,
+        time: new Date().toLocaleTimeString([], {hour: "2-digit", minute:"2-digit"})
     };
     socket.emit('sendingMsgEvent', data);
     rendermsg(data, "SENT")
-  
 })
+
+
+function removeElement() {
+    const myMsg = document.querySelector(".sent");
+    myMsg.addEventListener('dblclick', (e) => {
+         e.target.remove()
+    });
+}
+
 
 function rendermsg(data, typeOfMsg) {
     const msgdiv = document.createElement("div");
-    msgdiv.innerText = `${data.username} : ${data.message}`
+    const span = document.createElement("span");
+    const paragraph = document.createElement("p");
+    paragraph.innerText = `${data.username} : ${data.message} `
+    span.innerText = data.time;
     if (typeOfMsg == 'SENT') {
         msgdiv.setAttribute('class', 'message sent') 
     } else {
         msgdiv.setAttribute('class', 'message reciev') 
     }
+    msgdiv.appendChild(paragraph);
+    msgdiv.appendChild(span);
     messagecontainerr.appendChild(msgdiv);
     msgInput.value = "";
+    removeElement()
 }
 
 socket.on('spreadmsg', data => {
